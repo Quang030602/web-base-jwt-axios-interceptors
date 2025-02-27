5e0da45d0a409c706b546281902d2473ca4a8db0
Quang030602 <minhquang030602@gmail.com>
2025-02-27 10:59:18 +0700

    config Axios Interceptors

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,22 +9,18 @@ import Alert from '@mui/material/Alert'
 import { useForm } from 'react-hook-form'
 import Typography from '@mui/material/Typography'
 import TrungQuanDevIcon from '../assets/trungquandev-logo.png'
-import axios from 'axios'
 import { toast } from 'react-toastify'
 import { API_ROOT } from '~/utils/constants'
+import authorizedAxiosInstance from '~/utils/authorizedAxios'
 
 function Login() {
   const { register, handleSubmit, formState: { errors } } = useForm()
 
   const submitLogIn = async (data) => {
-    console.log('submit login: ', data)
-    try {
-      const res = await axios.post(`${API_ROOT}/v1/users/login`, data)
-      console.log(res.data)
-      toast.success(res.data?.message)
-    } catch (error) {
-      toast.error(error.response?.data?.message || error?.message)
-    }
+   
+    const res = await authorizedAxiosInstance.post(`${API_ROOT}/v1/users/login`, data)
+    toast.success(res.data?.message)
+    
   }
 
   return (
@@ -48,8 +44,8 @@ function Login() {
             </Box>
             <Box sx={{ display: 'flex', justifyContent: 'center', color: theme => theme.palette.grey[500] }}>
               <Box>
-                <Typography>Hint: trungquandev.official@gmail.com</Typography>
-                <Typography>Pass: trungquandev@123</Typography>
+                <Typography>Hint: minhquang030602@gmail.com</Typography>
+                <Typography>Pass: Quang030602</Typography>
               </Box>
             </Box>
             <Box sx={{ padding: '0 1em 1em 1em' }}>
--- src/utils/authorizedAxios.js
+++ src/utils/authorizedAxios.js
@@ -1,1 +1,38 @@
-// Author: TrungQuanDev: https://youtube.com/@trungquandev
+import axios from 'axios';
+import { toast } from 'react-toastify'
+
+
+// khởi tạo 1 đối tưởng Axios mục đích để custom và cấu hình chung cho dự án
+
+let authorizedAxios = axios.create()
+
+// thời gian chờ tối đa của 1 request
+authorizedAxios.defaults.timeout = 1000*60*10
+// withCredentials: true cho phép axios tự động đính kèm và  gửi cookie trong request tới BE
+// phục vụ trường hợp nếu chúng ta sử dụng JWT Tokens ( refresh & access) theo cơ chế httpOnly Cookie
+
+// authorizedAxios.defaults.withCredentials = true
+
+// can thiệp vào giữa những cái request API
+authorizedAxios.interceptors.request.use((config) => {
+        return config
+    },(error) => {
+        return Promise.reject(error)
+    }
+)
+// can thiệp vào giữa những cái response nhận về từ API
+authorizedAxios.interceptors.request.use((response) => {
+        return response
+    },(error) => {
+        console.log('error: ', error)
+        if (error?.response?.status !== 410) {
+            // xử lý logout
+            toast.error(error.response?.data?.message || error?.message)
+        }
+
+        return Promise.reject(error)
+    }
+)
+
+export default authorizedAxiosInstance
+
